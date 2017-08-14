b28b91b269dbf64fbef6b4848b05c0730e1b7b85
Guillaume Gardey <glinmac@gmail.com>
2017-08-14 21:20:52 +0100

    Externalize table name

--- spring-boot-hbase/src/main/java/ltd/cylleneworks/sandbox/springboot/hbase/UserRepository.java
+++ spring-boot-hbase/src/main/java/ltd/cylleneworks/sandbox/springboot/hbase/UserRepository.java
@@ -5,6 +5,7 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.hadoop.hbase.HbaseTemplate;
 import org.springframework.data.hadoop.hbase.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -19,7 +20,8 @@ public class UserRepository {
     @Autowired
     private HbaseTemplate hbaseTemplate;
 
-    private String tableName = "sandbox:users";
+    @Value("${app.users.hbase.table}")
+    private String tableName;
 
     private static byte[] cfData = Bytes.toBytes("d");
     private static byte[] qFullname = Bytes.toBytes("fullname");
@@ -27,11 +29,10 @@ public class UserRepository {
     private static byte[] qDob = Bytes.toBytes("dob");
 
     public Optional<User> find(String username) {
-        logger.info("SSS");
+
         return hbaseTemplate.get(tableName, username, new RowMapper<Optional<User>>() {
             @Override
             public Optional<User> mapRow(Result result, int rowNum) throws Exception {
-                logger.info("XXSSS");
                 if (result.size() > 0) {
                     return Optional.of(new User(Bytes.toString(result.getRow()),
                             Bytes.toString(result.getValue(cfData, qFullname)),
