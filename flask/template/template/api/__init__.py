37f02a6f257d2686ec206f79935de82787dd9554
Guillaume Gardey <glinmac@gmail.com>
2018-01-06 10:52:50 +0000

    Small update to template

--- flask/template/template/api/__init__.py
+++ flask/template/template/api/__init__.py
@@ -2,6 +2,8 @@ from flask import Blueprint
 
 api_app = Blueprint('main',
                     __name__,
-                    template_folder='templates')
+                    template_folder='templates',
+                    static_folder='static',
+                    static_url_path='/api-static')
 
 from . import app
